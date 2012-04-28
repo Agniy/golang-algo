fe82cc9f17d919e1db1c7922da9fcd53b13dec67
slonnik <slonnik@gmail.com>
2012-04-28 18:58:22 +0400

    implemet merge sort

--- src/pkg/algo/sort.go
+++ src/pkg/algo/sort.go
@@ -32,7 +32,57 @@ func InsertionSort(input []interface{}, eq Equals) []interface{}{
 }
 
 func MergeSort(input []interface{}, eq Equals) []interface{}{
-	return nil;
+	
+	split(0, len(input) - 1, input, eq);
+	return input;
+}
+
+func split(start int, end int, input []interface{}, eq Equals){
+	length := end -start +1
+	merge(start, start + length/2, start + length, input, eq)
+}
+
+/** merge two sorted subsets, where
+* whole unsorted array: A[start, end]
+* sorted subset 1: A[start, media]
+* sorted subset 2: A[media+1, end]
+* and start <= media <= end
+* @returns - merged array
+*/
+func merge(start int, media int, end int, input []interface{}, eq Equals){
+	
+	l := media -start + 1; //left hand length
+	r := end - media; //right hand length
+	
+	if (l != 1) && (r != 1){
+		split(start, media, input, eq);
+		split(media +1, media + 1, input, eq);
+	}	
+	
+	var left []interface{} = make([]interface{}, l); //left hand
+	var right []interface{} = make([]interface{}, r); //right hand
+	
+	left = input[start:media]; //copy the first subset into the left hand
+	right = input[media+1:end]; //copy teh second subset into the right hand
+	
+	/** start merging*/
+	var i, j, k int = start, media, start 
+	for ;(i < media) && (j < end); k++{
+		if eq(left[i], right[j]) == -1 {
+			//input[i] < input[j], we take input [i] 
+			input[k] = left[i];
+			i++;
+		} else {
+			input[k] = right[j];
+			j++;
+		}
+	}
+	
+	if i == media {
+		copy(input[k:],right[j:end]);
+	}else{
+		copy(input[k:],right[i:media]);
+	}
 }
 
 /** swap elements in input*/
